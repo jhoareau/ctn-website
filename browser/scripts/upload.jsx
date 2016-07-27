744e5badce625f9f04e94bc67215905c9876ae72
Julien Hoareau <jhoareau@ec-lyon.fr>
2016-07-27 10:43:21 +0200

    Mediapiston upload

--- browser/scripts/upload.jsx
+++ browser/scripts/upload.jsx
@@ -6,18 +6,22 @@ class Upload extends React.Component {
   }
   render() {
     return (
-        <i className="material-icons">cloud_upload</i>
+      <div className="upload-container">
+        <div className="uploadBox">
+          <i className="material-icons">cloud_upload</i><br/>
+          Vidéo
+        </div>
+        <div className="uploadBox">
+          <i className="material-icons">cloud_upload</i><br/>
+          Miniature
+        </div>
+      </div>
     );
   }
 }
 Upload.defaultProps = {
-  links: [
-          { title: "Mediapiston", href: '/mediapiston' },
-          { title: "Matériel", href: '/pret-matos' },
-          { title: "A propos", href: '/a-propos' },
-          { title: "Admin", href: '/ctn-asso' },
-          { title: "Déconnexion", href: '/logout', logout: true },
-         ]
+  initialVideo: [],
+  initialThumb: []
 };
 
 export default Upload;
