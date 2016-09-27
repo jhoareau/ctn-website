1a90a254dd08733d8efec3795149976b67f32ef4
ADJM <adejesus@ec-lyon.fr>
2016-09-27 19:56:31 +0200

    Ajout fonctions mongo comments

--- models/comment.js
+++ models/comment.js
@@ -8,3 +8,44 @@ let commentSchema = new mongoose.Schema({
 
 let Comment = mongoose.model('Comment', commentSchema);
 exports.model = Comment;
+
+exports.create = (infos, callback) => {
+    let schema = {
+      text: infos.text,
+      user: infos.user
+    }
+    let newCommment = new Comment(schema);
+    newCommment.save((err, result) => {
+      if (err) throw new Error("Erreur lors de la création du nouveau commentaire.");
+      callback(result);
+    })
+}
+
+exports.return = (id, callback) => {
+  Comment.findById(id, (err, comment) => {
+    if (err) throw new Error('Erreur lors de la récupération du commentaire.');
+    callback(comment);
+  });
+}
+
+//MOCHE, TODO regrouper les deux fct d'update en une seule
+exports.updateVotes = (id, up, callback) => {
+  Comment.findByIdAndUpdate(id, {$inc: {votes: up ? 1 : -1}}, (err, result) => {
+    if (err) throw new Error('Erreur lors de la mise à jour du nombre de votes du commentaire.');
+    callback(result);
+  });
+}
+
+exports.updateText = (id, update, callback) => {
+  Comment.findByIdAndUpdate(id, {$set: update}, (err, result) => {
+    if (err) throw new Error('Erreur lors de la mise à jour du commentaire.');
+    callback(result);
+  });
+}
+
+exports.delete = (id, callback) => {
+  Comment.findByIdAndRemove(id, (err, result) => {
+    if (err) throw new Error('Erreur lors de la suppression du commentaire.');
+    callback(result);
+  });
+}
